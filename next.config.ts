b9a30c15b9e9ca05eb259183f921afb15386e5c0
Gabriel Costa Luiz <gabrielbragacostaluiz@gmail.com>
2026-04-07 11:43:38 -0300

    fix: fixed embeed spotify

--- next.config.ts
+++ next.config.ts
@@ -21,20 +21,20 @@ const securityHeaders = [
     key: 'Referrer-Policy',
     value: 'origin-when-cross-origin'
   },
-  {
-    key: 'Content-Security-Policy',
-    value: `
-      default-src 'self';
-      script-src 'self' 'unsafe-inline' 'unsafe-eval' *.vercel-scripts.com;
-      style-src 'self' 'unsafe-inline' fonts.googleapis.com;
-      img-src 'self' blob: data: images.unsplash.com res.cloudinary.com;
-      font-src 'self' fonts.gstatic.com data:;
-      connect-src 'self' *.vercel.app viacep.com.br;
-      media-src 'self';
-      object-src 'none';
-      frame-src 'none';
-    `.replace(/\s{2,}/g, ' ').trim()
-  }
+{
+  key: 'Content-Security-Policy',
+  value: `
+    default-src 'self';
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' *.vercel-scripts.com https://embed-cdn.spotifycdn.com https://open.spotify.com;
+    style-src 'self' 'unsafe-inline' fonts.googleapis.com;
+    img-src 'self' blob: data: images.unsplash.com res.cloudinary.com https://i.scdn.co https://*.spotifycdn.com https://*.scdn.co;
+    font-src 'self' fonts.gstatic.com data:;
+    connect-src 'self' *.vercel.app viacep.com.br https://*.spotify.com;
+    media-src 'self' blob: data: https://*.spotifycdn.com;
+    object-src 'none';
+    frame-src 'self' https://open.spotify.com https://embed.spotify.com;
+  `.replace(/\s{2,}/g, ' ').trim()
+}
 ];
 
 const nextConfig: NextConfig = {
